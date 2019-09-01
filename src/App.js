9d3cd4860ed496c42789402858b93e67a9e34409
keithlem <keithlem9@gmail.com>
2019-09-01 19:58:57 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './components/home';
+import About from './components/about';
+import Header from './components/header';
+import Contact from './components/contact';
+import Services from './components/services';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+        <Home />
+
+        <About />
+        <Services />
+
+        <Contact />
+      </div>
+    );
+  }
 }
 
 export default App;
